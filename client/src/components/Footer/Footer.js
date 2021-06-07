939580765efd3e879e012d8a04ade208d46fce24
meerawadher <wadher.meera@gmail.com>
2021-06-07 18:40:57 +0530

    Changes in Footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -23,20 +23,21 @@ const Footer = () => {
                     <div className="social">
 
                         <h3 >Connect With Us</h3>
-                        <a href="https://www.linkedin.com/company/ieee-vjti"><i className="fab fa-linkedin-in fa-2x"></i></a>
-                        <a href="https://www.instagram.com/ieeevjti/"><i className="fab fa-instagram fa-2x"></i></a>
-                        <a href="ieeevjtiofficial@gmail.com"><i className="fas fa-envelope-square fa-2x"></i></a>
-                        <a href="https://www.youtube.com/channel/UCXkEZSsh_RWiBfrJIPY27vg"><i className="fab fa-youtube fa-2x"></i></a>
-                        <p><a href="https://issuu.com/ieeevjti">Issuu</a>
+                      <a href="https://www.linkedin.com/company/ieee-vjti" target="_blank"><i className="fab fa-linkedin-in fa-2x"></i></a>
+                        <a href="https://www.instagram.com/ieeevjti/" target="_blank"><i className="fab fa-instagram fa-2x"></i></a>
+                        <a href="ieeevjtiofficial@gmail.com" target="_blank"><i className="fas fa-envelope-square fa-2x"></i></a>
+                        <a href="https://www.youtube.com/channel/UCXkEZSsh_RWiBfrJIPY27vg" target="_blank"><i className="fab fa-youtube fa-2x"></i></a>
+                      
+                        <p><a href="https://issuu.com/ieeevjti" target="_blank">Issuu</a>
                             <br />
-                            <a href="https://linktr.ee/ieeevjti">LinkTree</a>
+                            <a href="https://linktr.ee/ieeevjti" target="_blank">LinkTree</a>
                         </p>
 
 
                     </div>
                 </div>
                 <hr className="horizontal_line" />
-                <p className="text-center">Copyright &copy;IEEE VJTI 2021 </p>
+                <p className="text-center">Copyright &copy;IEEE VJTI 2021</p>
 
 
             </footer>
