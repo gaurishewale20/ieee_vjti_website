27a12e74b181569dd334abc977174b95a00eefab
Meera Wadher <54815182+Meera-W@users.noreply.github.com>
2021-07-02 21:17:24 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+
+  return req;
+});
 
 export const fetchEvents = () => API.get(`/events/all`);
-export const createEvent = (newEvent) => API.post('/events', newEvent);
+export const createEvent = (newEvent) => API.post("/events", newEvent);
+
+export const signIn = (adminData) => API.post("/admin/signin", adminData);
