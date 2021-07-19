52806914d7ee3e67bc0a1eee2edfb3996acb5d2f
Meera Wadher <54815182+Meera-W@users.noreply.github.com>
2021-07-19 09:54:56 +0530

    Changed serverside path from events/all to events

--- client/src/actions/events.js
+++ client/src/actions/events.js
@@ -1,10 +1,10 @@
 import { FETCH_ALL,CREATE } from '../constants/actionTypes';
 import * as api from '../api/index.js';
 
-export const getEvents = () => async (dispatch) => {
+export const getEvents = (page) => async (dispatch) => {
   try {
-    const { data } = await api.fetchEvents();
-
+    const { data } = await api.fetchEvents(page);
+console.log(data);
     dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error);
