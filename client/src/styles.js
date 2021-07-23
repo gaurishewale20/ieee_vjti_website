de8436a0dff2c6e40ca0243dd3e9dcc83afb9243
Meera Wadher <54815182+Meera-W@users.noreply.github.com>
2021-07-23 22:07:24 +0530

    Tried to set the Events Heading, it didn't work.
    Honestly, I need to work on my CSS.

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
+  heading:{
+display:'flex',
+align: 'center',
+marginLeft: '100px',
+paddingLeft:'100px'
+  },
   pagination: {
     borderRadius: 4,
     marginTop: '1rem',
