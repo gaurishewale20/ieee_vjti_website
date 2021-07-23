2670fffab8fee0b19873c27ddeb89c267bc81cde
Meera Wadher <54815182+Meera-W@users.noreply.github.com>
2021-07-23 22:06:27 +0530

    Update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,7 +1,8 @@
  import React from "react";
  import "./Home.css";
  import {purplewave, bluewave, Showcase_Image, AI, ML, IOT, Computation, Renewable_Energy, Mechatronics, Electric_Vehicles, Biotechnology, Open_Source, ieee_weekly1, ieee_weekly2, ieee_weekly3, Fostering_Tech } from './';
-  const Home = () => {
+  
+ const Home = () => {
   return (
     <>
       <section className="showcase">
