f5bf794899c3953b7b76e3464a84e4625c22955a
Meera Wadher <54815182+Meera-W@users.noreply.github.com>
2021-08-05 18:23:00 +0530

    Made Magazine Page responsive!

--- client/src/components/Magazine/Magazine.jsx
+++ client/src/components/Magazine/Magazine.jsx
@@ -4,6 +4,11 @@ import {mag_og} from "../Home/index"
 const Magazine = () => {
     return (
         <>
+         <div className="magazine_div text-center">
+        <h1>IEEE Annual Magazines</h1>
+        <h5 className="text-secondary">Click on them to Read!</h5>
+           
+    
         {/* <div className='magazine'>
             <div style={{position: 'relative', width: '100%', height: '0', paddingTop: '141.4286%',
  paddingBottom: '48px', boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)', marginTop: '1.6em', marginBottom: '0.9em', overflow: 'hidden',
@@ -15,7 +20,13 @@ const Magazine = () => {
 <a href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAD98MmmtKs&#x2F;view?utm_content=DAD98MmmtKs&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" target="_blank" rel="noopener">IEEE VJTI Annual Newsletter 2020</a> 
         </div> */}
         <div className="magazine text-center">
+        <h6>2021</h6>
            <a href="https://www.canva.com/design/DAD98MmmtKs/6oqGF4C6PHlJg8AdFgJ7Wg/view?utm_content=DAD98MmmtKs&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target="_blank"><img className="img" src={mag_og}></img></a>
+        </div>
+        <div className="magazine text-center">
+        <h6>2020</h6>
+           <a href="https://www.canva.com/design/DAD98MmmtKs/6oqGF4C6PHlJg8AdFgJ7Wg/view?utm_content=DAD98MmmtKs&utm_campaign=designshare&utm_medium=embeds&utm_source=link" target="_blank"><img className="img" src={mag_og}></img></a>
+        </div>
         </div>
         </>
     )
