06ffe3605c1234c6f9a10e9356f0e6e343735b15
Meera Wadher <54815182+Meera-W@users.noreply.github.com>
2021-08-05 15:58:16 +0530

    Update Membership.js

--- client/src/components/Membership/Membership.js
+++ client/src/components/Membership/Membership.js
@@ -31,7 +31,7 @@ const Membership = () => {
                         </div>
                         <div className="card-body">
                             <h4 className="card-title">Conference Organisers</h4>
-                            <p className="card-text text-muted">Organise insightful conferences with IEEE's guidance and training and be a part of their conferences commitee</p>
+                            <p className="card-text text-muted">Organise insightful conferences with IEEE's guidance and training and be a part of their conferences commitee.</p>
                         </div>
                         <div className="card-footer text-end">
                             <a href="https://www.ieee.org/conferences_events/conferences/organizers/organizers_index.html?WT.mc_id=" className="btn btn-primary btn-responsive" target='_blank'>More Details</a>
@@ -45,7 +45,7 @@ const Membership = () => {
                         </div>
                         <div className="card-body">
                             <h4 className="card-title">Education</h4>
-                            <p className="card-text text-muted">Let IEEE help your education with access to tons of resources, scholarships and access to award program opportunities</p>
+                            <p className="card-text text-muted">Let IEEE help your education with access to tons of resources, scholarships and access to award program opportunities.</p>
                         </div>
                         <div className=" card-footer text-end">
                             <a href="https://www.ieee.org/education/index.html" className="btn btn-primary btn-responsive" target='_blank'>More Details</a>
@@ -61,7 +61,7 @@ const Membership = () => {
                         </div>
                         <div className="card-body">
                             <h4 className="card-title">Like-Minded Students</h4>
-                            <p className="card-text text-muted">Network with students united by a common desire to continuously learn, interact, collaborate, and innovate</p>
+                            <p className="card-text text-muted">Network with students united by a common desire to continuously learn, interact, collaborate, and innovate.</p>
                         </div>
                         <div className=" card-footer text-end">
                             <a href="https://www.ieee.org/membership_services/membership/students/index_students.html?WT.mc_id=" className="btn btn-primary btn-responsive" target='_blank'>More Details</a>
@@ -76,7 +76,7 @@ const Membership = () => {
                         </div>
                         <div className="card-body">
                             <h4 className="card-title">FAQs</h4>
-                            <p className="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content</p>
+                            <p className="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>   
                         <div className="card-footer text-end">
                             <a href="https://supportcenter.ieee.org/app/home?utm_source=hp&utm_campaign=faq&utm_medium=how&utm_term=support%20center%2C%20faqs&WT.mc_id=" className="btn btn-primary btn-responsive" target='_blank'>More Details</a>
@@ -91,7 +91,7 @@ const Membership = () => {
                         </div>
                         <div className="card-body">
                             <h4 className="card-title">Volunteers</h4>
-                            <p className="card-text text-muted"> Enrich your technical knowledge and gain valuable management and leadership skills</p>
+                            <p className="card-text text-muted"> Enrich your technical knowledge and gain valuable management and leadership skills.</p>
                         </div>
                         <div className="card-footer text-end">
                             <a href="https://www.ieee.org/about/volunteers/volunteer_index.html?WT.mc_id=" className="btn btn-primary btn-responsive" target='_blank'>More Details</a>
@@ -122,7 +122,7 @@ const Membership = () => {
                         </div>
                         <div className="card-body">
                             <h4 className="card-title">Media and Journalists</h4>
-                            <p className="card-text text-muted"></p>
+                            <p className="card-text text-muted">Learn how to request a media interview, see IEEE’s mission, learn about its leadership, find fact sheets, read recent IEEE news, and more.</p>
                         </div>
                         <div className="card-footer text-end">
                             <a href="https://www.ieee.org/about/news/media_kit/media_kit_index.html?WT.mc_id=" className="btn btn-primary btn-responsive" target='_blank'>More Details</a>
@@ -137,7 +137,7 @@ const Membership = () => {
                         </div>
                         <div className="card-body">
                             <h4 className="card-title">Research Papers</h4>
-                            <p className="card-text text-muted">Get access to the industry's most essential technical information and research papers</p>
+                            <p className="card-text text-muted">Get access to the industry's most essential technical information and research papers.</p>
                         </div>
                         <div className="card-footer text-end">
                             <a href="https://ieeexplore.ieee.org/Xplore/home.jsp?utm_source=hp&utm_campaign=xpl_research&utm_medium=how&utm_term=Xplore%20researchers&WT.mc_id=" className="btn btn-primary btn-responsive" target='_blank'>More Details</a>
@@ -168,7 +168,7 @@ const Membership = () => {
                         </div>
                         <div className="card-body pb-0">
                             <h4 className="card-title">Employers and Job Seekers</h4>
-                            <p className="card-text text-muted">Get the opportunity to recruit highly qualified IEEE member candidates and leverage employment opportunities</p>
+                            <p className="card-text text-muted">Get the opportunity to recruit highly qualified IEEE member candidates and leverage employment opportunities.</p>
                         </div>
                         <div className="card-footer text-end pt-0">
                             <a href="https://www.ieee.org/education_careers/careers/employers_job_seekers.html?WT.mc_id=" className="btn btn-primary btn-responsive" target='_blank'>More Details</a>
@@ -183,7 +183,7 @@ const Membership = () => {
                         </div>
                         <div className="card-body">
                             <h4 className="card-title">Young Professionals</h4>
-                            <p className="card-text text-muted">Elevate your professional image and expand your global network by connecting with peers</p>
+                            <p className="card-text text-muted">Elevate your professional image and expand your global network by connecting with peers.</p>
                         </div>
                         <div className="card-footer text-end">
                             <a href="https://www.ieee.org/membership_services/membership/young_professionals/index_young_professionals.html?WT.mc_id=" className="btn btn-primary btn-responsive" target='_blank'>More Details</a>
