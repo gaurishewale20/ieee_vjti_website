b3269caa714be277c66ba0055a9542c204346e29
gaurishewale <gaurishewale201201@gmail.com>
2021-08-19 23:45:45 +0530

    Working with spinner

--- client/src/components/ContactUs/Submit.js
+++ client/src/components/ContactUs/Submit.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Loader from '../Loader/Loader';
 
 const Submit = () => {
     return <div >
+        <center>
+        <Loader/>
+        </center>
         <p> Thank You for Your Submission. </p>
         <a href="/"> Return to home</a>
     </div>
---
+++ client/src/components/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Loader.model.css";
+const Loader = () =>{
+    return (
+    <div className="bars">
+
+    </div>);
+
+}
+
+export default Loader;
