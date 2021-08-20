7af56775ffcf3aee1322dc6e594e59a2e5808b9c
meerawadher <wadher.meera@gmail.com>
2021-08-20 23:57:01 +0530

    Minor Changes in Error Form

--- client/src/components/Error404/Error404.js
+++ client/src/components/Error404/Error404.js
@@ -1,16 +1,16 @@
 import React from "react";
-import "./Error404.css";
+import "./Error404.model.css";
 import Error404Image from '../../assets/images/Error404.png'
 import {Link} from 'react-router-dom';
 
 const Error404 = () => {
 
     return (
-        <>
-            <img src={Error404Image} class="img-fluid mt-5 mx-auto d-block" alt="404 Page Image" />
-            <p class="display-4 text-center mt-5">OOPS! Webpage Doesn't Exist</p>
-            <Link to="/"><p class="text-center">Go back to IEEE VJTI's Homepage</p></Link>
-        </>
+        <div style={{minHeight: "70vh"}}>
+            <img src={Error404Image} className="img-fluid mt-5 mx-auto px-5 d-block" alt="404 Page Image" />
+            <p className=" text-center mt-5">OOPS! Webpage Doesn't Exist</p>
+            <Link to="/"><p className="text-center">Go back to IEEE VJTI's Homepage</p></Link>
+        </div>
     );
 };
 
